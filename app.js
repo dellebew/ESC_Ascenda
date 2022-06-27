24da306c86b54c00176e490e2c9e00c5149e6b22
Mason <yuchen_guo@mymail.sutd.edu.sg>
2022-06-27 17:00:50 +0800

    api.js abe to retrieve and store data

--- app.js
+++ app.js
@@ -3,18 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var { MongoClient } = require("mongodb");
-var fs = require('fs');
-
-const uri =  "mongodb+srv://ringdong2022:Abcdef2022@cluster0.8cytz.mongodb.net/?retryWrites=true&w=majority";
-var client = new MongoClient(uri);
-client.connect();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api');
 
 var app = express();
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -29,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api',apiRouter)
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -46,8 +43,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(80,()=>{
-  console.log("server listening");
-})
 
 module.exports = app;
--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,58 @@
+// retrieve and store in db
 var express = require('express');
 var router = express.Router();
-var url = require('url');
-var request = require('request');
+const https = require('https')
+
+var { MongoClient } = require("mongodb");
+const uri =  "mongodb+srv://ringdong2022:Abcdef2022@cluster0.8cytz.mongodb.net/?retryWrites=true&w=majority";
+var client = new MongoClient(uri);
+
 
 const baseUrl = 'https://hotelapi.loyalty.dev/api/hotels/'
 const id = 'diH7'
-router.get('/', function(req, res, next) {
-  request({
-    uri: url.resolve(baseUrl,id)
-  }).pipe(res);
-});
+
+const value = Array();
+router.get('/', function(req, reso, next) {
+
+    https.get(baseUrl+id, res => {
+    let data = '';
+    res.on('data', chunk => {
+        data += chunk;
+    });
+    res.on('end', () => {
+        data = JSON.parse(data);
+        value.push(data.amenities_ratings);
+        reso.send(value[0][0].name)
+        run(value[0]).catch(console.dir);
+    })
+    }).on('error', err => {
+    console.log(err.message);
+    })
+
+    
+  });
+
+
+async function run(docs) {
+  try {
+    await client.connect();
+
+    // database and collection code goes here
+    const db = client.db("ascenda-hotel-booking");
+    const coll = db.collection("planets");
+    // const coll = db.collection("destinations");
+
+    // insert data
+    const result = await coll.insertMany(docs);
+    console.log(result);
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+
+
 
 module.exports = router;
