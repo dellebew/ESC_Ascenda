f441db8b4fcb7701d6d5719f86b41178257c3d47
bloomspx <bloomspx@gmail.com>
2022-06-30 02:29:01 +0800

    updated market-rate-cards

--- src/components/roomCard/RoomCard.jsx
+++ src/components/roomCard/RoomCard.jsx
@@ -1,34 +1,36 @@
+import RatesCard from "../ratesCard/RatesCard"
 import "./roomCard.css"
 
 const RoomCard = () => {
     return (
         <div className="roomCard">
-            <div className="room--desc">
-                <img
-                    src="https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/78478012.jpg?k=3d82d2321cf8e1c9de658b7720a1d835f015d7ac55fa782be05d69db0a97c8b5&o=&hp=1"
-                    alt=""
-                    className="room--image"
-                />
-                <div className="room--details">
-                    <h3 className="room--name">Deluxe Room, 1 King Bed</h3>
-                    <span>Sleeps 3 people (including up to 2 children)</span>
-                </div>
-            </div>
-            <div className="room--options">
-                <div className="room--header">
-                    <span>Options</span>
-                    <span>Today’s Price including taxes and fees</span> 
-                </div>
-                <div className="room--body">
-                    <div className="room--description"> 
-                        <span>Fully Refundable</span>
-                        <span><small>Until 29/06/2022</small></span>
+            <div className="room--container">
+                <div className="room-body">
+                    <img
+                        src="https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/78478012.jpg?k=3d82d2321cf8e1c9de658b7720a1d835f015d7ac55fa782be05d69db0a97c8b5&o=&hp=1"
+                        alt=""
+                        className="room--image"
+                    />
+                    <div className="room--details">
+                        <h3 className="room--normalized">Deluxe Room, 1 King Bed</h3>
+                        <span className="free-cancellation">Free Cancellation</span>
+                        <p className="description">This is a very long description of the room.</p> 
                     </div>
-                    <div className="room--pricing"> 
+                </div>
+                <div className="room--pricing"> 
+                        <span className="website">EAN</span>
                         <span className="price">$123</span>
                         <span className="room">for 1 room for 1 night</span>
                         <button className="reserve">Reserve</button>
-                    </div>
+                </div>
+            </div>
+            <div className="room--options">
+                <div className="room--header">Market Rates</div>
+                <div className="room--body">
+                    <RatesCard />
+                    <RatesCard />
+                    <RatesCard />
+                    <RatesCard />
                 </div>
 
             </div>
--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -5,6 +5,7 @@ import RoomCard from '../../components/roomCard/RoomCard'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
 import SearchBar from '../../components/searchBar/SearchBar'
+import axios from "axios"
 
 const Hotel = () => {
     return (
@@ -13,11 +14,11 @@ const Hotel = () => {
             <SearchBar />
             <div className='hotel--container'>
                 <div className='hotel--wrapper'>
+
                     <div className="hotel--header">
                         <div className="hotel--summary">
-                            <h1 className='hotel--title'>Grand Copthorne Waterfront Singapore</h1>
+                            <h1 className='hotel--name'>Grand Copthorne Waterfront Singapore</h1>
                             <div className='hotel--address'>
-                                <FontAwesomeIcon icon={faLocationDot}/>
                                 <span>392 Havelock Road, Robertson Quay, 169663 Singapore, Singapore</span>
                             </div>
                             <span className='hotel--shortdesc'>Luxury hotel with 1 outdoor pools, connected to a shopping center, near Orchard Road </span>
@@ -28,20 +29,52 @@ const Hotel = () => {
                             <button className="hotel--choose">Choose Room</button>
                         </div>
                     </div>
-                    <div className="hotel--images">
-                        <img src="https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/249556539.jpg?k=dd3501ee1cd64e6d54cf8a6459119080a2b95cee06fe4a3be5153454fa4293de&o=&hp=1"/>
+                    
+                    <div className="hotel--body">
+                        <img className="hotel--images" 
+                            src="https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/249556539.jpg?k=dd3501ee1cd64e6d54cf8a6459119080a2b95cee06fe4a3be5153454fa4293de&o=&hp=1"/>
+                        <div className='hotel--details'>
+                            <div className="hotel--location">
+                                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.wired.com%2Fphotos%2F5a6a61938c669c70314b300d%2Fmaster%2Fw_2400%2Cc_limit%2FGoogle-Map-US_10.jpg&f=1&nofb=1"/>
+                                <div className="hotel--state">
+                                    <FontAwesomeIcon icon={faLocationDot}/>
+                                    City, State, Postal Code
+                                </div>
+                            </div>
+                            <div className="hotel--rating"> 
+                                <span>
+                                    <button>8.9</button>
+                                    Excellent
+                                </span>
+                            </div>
+                        </div>
                     </div>
-                    <div className='hotel--details'>
-                        <p className='hotel--desc'>
-                            <h2>Hotel Description</h2> 
+
+                    <div className='hotel--body'>
+                        <p className='hotel--description'>
+                            <h2>Hotel Description</h2> <br/>
                             Grand Copthorne Waterfront is located along the Singapore River. A 5-minute drive from Orchard Road, the hotel offers an outdoor pool, 4 restaurants and free parking. Free WiFi is available throughout the hotel.
                             Luxurious rooms come with floor-to-ceiling windows that offer city or river views. Amenities include a flat-screen TV with cable channels that include local and international channels in languages such as Chinese and English. Free toiletries include toothbrush and toothpaste.
                             Guests can exercise at the on-site gym facility. Multi-lingual staff can assist arrangements for sightseeing tours at the on-site tour desk, which supports languages such as English, Malay and Chinese. For convenience, laundry and dry cleaning services are also provided. Newspapers are available in English and Chinese.
                         </p>
-                        <div className="hotel--location">
-                            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.wired.com%2Fphotos%2F5a6a61938c669c70314b300d%2Fmaster%2Fw_2400%2Cc_limit%2FGoogle-Map-US_10.jpg&f=1&nofb=1"/>
+                        <div className="hotel--details2">
+                            <h3>Contact Details</h3>
+                            <div className="hotel--contact">
+                                <span className='hotel--phone'><b>Telephone:</b> (country-code) 00000000</span>
+                                <span className='hotel--fax'><b>Fax:</b> (country-code) FAX</span>
+                                <span className='hotel--email'><b>Email:</b> loser@email.com</span>
+                            </div>
+                            <h3>Amenities</h3>
+                            <div className='hotel--amenities'>
+                                <span>Free WiFi</span>
+                                <span>Free parking</span>
+                                <span>Non-smoking</span>
+                                <span>Air Conditioning</span>
+                            </div>
                         </div>
+                        
                     </div>
+
                     <div className='hotel--rooms'>
                         <h2>Room Choices</h2>
                         <RoomCard />
