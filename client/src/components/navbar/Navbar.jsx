37ef165cb60bdbe59de7eb4fb01aae52b7167ad6
-bottomfragger- <75031614+Ignite99@users.noreply.github.com>
2022-07-26 09:33:22 +0800

    small change to className

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -3,22 +3,22 @@ import "./navbar.css"
 const NavBar = () => {
     
     return (
-        <nav class="menu">
+        <nav className="menu">
             <ol>
-                <li class="menu-item"><a href="/" >Home</a></li>
-                <li class="menu-item"><a href="#/">About</a></li>
-                <li class="menu-item"><a href="#/">Services</a>
-                <ol class="sub-menu">
-                <li class="menu-item"><a href="/">Destination Search</a></li>
-                <li class="menu-item"><a href="/hotels">Hotel Search</a></li>
+                <li className="menu-item"><a href="/" >Home</a></li>
+                <li className="menu-item"><a href="#/">About</a></li>
+                <li className="menu-item"><a href="#/">Services</a>
+                <ol className="sub-menu">
+                <li className="menu-item"><a href="/">Destination Search</a></li>
+                <li className="menu-item"><a href="/hotels">Hotel Search</a></li>
             </ol>
             </li>
-            <li class="menu-item">
+            <li className="menu-item">
                 <a href="#/">Currency</a>
-                <ol class="sub-menu">
-                <li class="menu-item"><a href="#/">SGD</a></li>
-                <li class="menu-item"><a href="#/">JPY</a></li>
-                <li class="menu-item"><a href="#/">EUR</a></li>
+                <ol className="sub-menu">
+                <li className="menu-item"><a href="#/">SGD</a></li>
+                <li className="menu-item"><a href="#/">JPY</a></li>
+                <li className="menu-item"><a href="#/">EUR</a></li>
                 </ol>
             </li>
             </ol>
